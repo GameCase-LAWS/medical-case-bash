4a7b61b3f86cc2a45e8dac76004b6e9003eaf4ce
Alexandre Aragão <alexr.aragao@gmail.com>
2021-01-25 14:59:07 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Medical Case Bash
+
+## Gerar projeto
+O projeto Medical Case é gerado a partir do projeto oficial "Dental Case". Para clonar o projeto use o comando bash:
+
+```sh
+bash build.sh
+```
+
+## Repositórios
+- [Dental Case App](https://github.com/GameCase-LAWS/dental-case-app)
+- [Dental Case Web](https://github.com/GameCase-LAWS/dental-case-web)
+- [Medical Case](https://github.com/GameCase-LAWS/medical-case-bash)
+- [Clinical Case](https://github.com/GameCase-LAWS/clinical-case-bash)
+
+## Equipe de programação
+- [Alexandre Aragão*](https://www.linkedin.com/in/alexr-aragao/)
+- [Antônio Tadeu]()
+- [Claudiny Brito](https://www.linkedin.com/in/claudiny-priscila-lopes-brito-6985b1190/)
+- [Leandro Massetti](https://www.linkedin.com/in/leandro-massetti/)
+- [Nikolas Oliveira]()
+- [Nathyane Moreno](https://www.linkedin.com/in/nathyane-moreno-217432a8/)
+
+## Coordenadores
+- [Alana Oliveira](https://www.linkedin.com/in/alanaslz/)
+- [Mario Meirelles]()
---
+++ build.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+echo 'Select an option and press Enter: '
+options=("web" "app")
+select opt in "${options[@]}"; do
+  case $opt in
+    "web")
+      PROJECT_NAME="dental-case-web"
+      break
+      ;;
+    "app")
+      PROJECT_NAME="dental-case-app"
+      break
+      ;;
+  esac
+done
+
+# Delete previous project folder
+rm -f -r $PROJECT_NAME
+
+# Clone 'Dental Case' git repository
+git clone https://github.com/GameCase-LAWS/$PROJECT_NAME.git
+
+if [ "$PROJECT_NAME" = "dental-case-app" ];
+then
+  # Replace 'dental' term for 'medical'
+  sed -i 's/dental/medical/g' $PROJECT_NAME/app.json
+  sed -i 's/Dental/Medical/g' $PROJECT_NAME/app.json
+  sed -i 's/DENTAL/MEDICAL/g' $PROJECT_NAME/app.json
+
+  # Substituir arquivos de configuração
+  cp -r files/config $PROJECT_NAME/app
+
+  # Substituir imagens
+  cp -r files/assets $PROJECT_NAME/app
+fi
+
+if [ "$PROJECT_NAME" = "dental-case-web" ];
+then
+  # Replace 'dental' term for 'medical'
+  sed -i 's/dental/medical/g' $PROJECT_NAME/package.json
+  sed -i 's/Dental/Medical/g' $PROJECT_NAME/package.json
+  sed -i 's/DENTAL/MEDICAL/g' $PROJECT_NAME/package.json
+
+  sed -i 's/Dental/Medical/g' $PROJECT_NAME/public/index.html
+
+  # Substituir favicon
+  cp -r files/assets/icons/favicon.png $PROJECT_NAME/public/favicon.png
+
+  # Substituir arquivos de configuração
+  cp -r files/config $PROJECT_NAME/src
+
+  # Substituir imagens
+  cp -r files/assets $PROJECT_NAME/src
+fi
+
+echo "Done! =)"
